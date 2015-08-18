8c68e78f416a8415e390fc9618772ed1db40bb50
robksawyer <robksawyer@gmail.com>
2015-08-17 23:23:03 -0700

    Trying to add more tests

--- test/controllers/actions/login.test.js
+++ test/controllers/actions/login.test.js
@@ -5,8 +5,28 @@ var waterlock = testHelper.waterlock_local;
 var login = waterlock.actions.login;
 
 describe('login action', function(){
+
   it('should exist', function(done){
     login.should.be.Function;
     done();
   });
+
+
+  it('should respond with error', function(done){
+    var req = {
+      method: 'PUT',
+      params: {
+         email: 'test@test.com',
+         password: 'testtesttest'
+      }
+    };
+    var res = {
+      json: function(code){
+         code.should.equal(200);
+         done();
+      }
+    };
+    login(req, res);
+  });
+
 });
